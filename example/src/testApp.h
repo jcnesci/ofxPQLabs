801744a179b7ba0b8b781c62394d73dd14ba593f
JC Nesci <jc.nesci@gmail.com>
2014-06-17 18:14:39 -0700

    Changed file structure to replicate that of ofxSoso (using examples and src folders). Also changed the app's base SDK to 10.8 (what I have).

---
+++ example/src/testApp.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxPQLabs.h"
+
+class testApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+	
+		ofxPQLabs connection;
+	void onSingleTouchGestureEvent(SingleTouchGestureEvent & event);
+	void onBigTouchEvent(BigTouchEvent & event);
+	void onSingleTouchMoveEvent(SingleTouchMoveEvent & event);
+
+	void onRotateEvent(RotateEvent & event);
+	void onRotatingEvent(RotatingEvent & event);
+
+	void onSecondTouchEvent(SecondTouchEvent & event);
+	
+	void onSplitEvent(SplitEvent & event);
+	void onSplitMoveEvent(SplitMoveEvent & event);
+	
+	void onParallelEvent(ParallelEvent & event);
+	void onParallelClickEvent(ParallelClickEvent & event);
+	void onParallelMoveEvent(ParallelMoveEvent & event);
+	
+	void onMultiEvent(MultiEvent & event);
+	void onMultiMoveEvent(MultiMoveEvent & event);
+	
+	void onGestureClearEvent(GestureClearEvent & event);
+	
+	void onTouchPoint(TouchPointEvent & event);
+};
